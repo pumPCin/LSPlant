b5e404cb7861574996e607dfe10f3521f604d952
Alex <pumpcin14@mail.ru>
2024-06-08 18:45:17 +0300

    Update test.cpp

--- test/src/main/jni/test.cpp
+++ test/src/main/jni/test.cpp
@@ -6,7 +6,6 @@
 #include "logging.h"
 
 import lsplant;
-import lsparself;
 
 #define _uintval(p)               reinterpret_cast<uintptr_t>(p)
 #define _ptr(p)                   reinterpret_cast<void *>(p)
@@ -63,7 +62,6 @@ JNI_OnLoad(JavaVM* vm, void* reserved) {
 #if !defined(__i386__)
     dobby_enable_near_branch_trampoline();
 #endif
-    lsparself::Elf art("/libart.so");
     lsplant::InitInfo initInfo{
             .inline_hooker = InlineHooker,
             .inline_unhooker = InlineUnhooker,
